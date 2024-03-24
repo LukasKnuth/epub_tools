4e8c45e7839900db8197a235868466f86bd7809c
Lukas Knuth <me@lknuth.dev>
2024-03-24 18:10:00 +0100

    Turn readable article into markdown to get rid of complicated markup

--- cleanup.js
+++ cleanup.js
@@ -1,6 +1,7 @@
 const { JSDOM } = require("jsdom")
 const { Readability } = require("@mozilla/readability")
 const prettier = require("prettier")
+const TurndownService = require("turndown")
 const fs = require("fs")
 
 const file = fs.readFileSync(process.argv[2])
@@ -12,34 +13,31 @@ const parsed = reader.parse()
 
 // Remove additional noise
 const article = JSDOM.fragment(parsed.content)
-article.querySelectorAll("div").forEach(e => e.replaceWith(...e.childNodes))
-article.querySelectorAll("figure").forEach(e => {
-	e.removeAttribute("data-imgstate")
-	const img = e.querySelector("img")
-	img.removeAttribute("data-dither")
-	img.removeAttribute("data-original")
-	img.removeAttribute("loading")
+article.querySelectorAll("img").forEach(e => {
 	// todo change img path to reflect relative in ebook
-	console.log("Used image: "+img.getAttribute("src"))
-	const caption = e.querySelector("figcaption")
-	const content = caption.querySelector("span")
-	caption.replaceChildren(...content.childNodes)
+	console.log("Used image: "+e.getAttribute("src"))
 })
 
+const td = new TurndownService({
+	headingStyle: "atx",
+	bulletListMarker: "-",
+	codeBlockStyle: "fenced"
+})
+td.keep(["table"])
+const out = td.turndown(article)
+
+
 // Setup scaffolding
-const dom = new JSDOM(`<?xml version="1.0" encoding="utf-8"?>
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" xml:lang="en-US">
-<head /><body epub:type="frontmatter"/>
-</html>`, {contentType: "application/xhtml+xml"})
-const document = dom.window.document
-const title = document.createElement("title")
-title.text = parsed.title
-document.head.append(title)
-const section = document.createElement("section")
-const header = document.createElement("header")
-// todo unwrap article into the thing directly.
-section.append(header, ...article.querySelector("article").childNodes)
-document.body.append(section)
+// TODO write YAML header with title, description, author, etz.
+const header = `---
+title: ${parsed.title}
+source: ${parsed.siteName}
+date: ${parsed.publishedTime}
+author: ${parsed.byline}
+description: ${parsed.excerpt}
+---
+
+`
 
 // Write result
-prettier.format(dom.serialize(), {parser: "html"}).then(pretty => fs.writeFileSync("out.xhtml", pretty))
+fs.writeFileSync("out.md", header+out)
