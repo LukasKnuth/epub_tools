9336dc8a9b5e070611d0e459fafba9b11051f106
Lukas Knuth <me@lknuth.dev>
2024-03-26 21:53:51 +0100

    Cleanup script can now process multiple files in a row

--- cleanup.js
+++ cleanup.js
@@ -33,9 +33,13 @@ description: ${readable.excerpt}
 `
 }
 
-const full_path = path.join(process.cwd(), process.argv[2])
-const file = fs.readFileSync(full_path)
-const simple = simplify(file)
-const output = generateFrontMatter(simple) + generateMarkdown(simple)
-const source_file = path.parse(full_path)
-fs.writeFileSync(path.join(source_file.dir, source_file.name + ".md"), output)
+for (const arg of process.argv.slice(2)) {
+	const full_path = path.join(process.cwd(), arg)
+	console.log(`Processing ${full_path}...`)
+	const file = fs.readFileSync(full_path)
+	const simple = simplify(file)
+	const output = generateFrontMatter(simple) + generateMarkdown(simple)
+	const source_file = path.parse(full_path)
+	fs.writeFileSync(path.join(source_file.dir, source_file.name + ".md"), output)
+	console.log("Done!")
+}
